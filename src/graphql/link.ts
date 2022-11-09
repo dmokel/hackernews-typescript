0aa9dd2cd0cd2c9e60bac273eb2218b546efc8dd
David Mokel <Moqqll.china@gmail.com>
2022-11-09 21:57:00 +0800

    feat(all): add orderBy argument

--- src/graphql/link.ts
+++ src/graphql/link.ts
@@ -1,4 +1,19 @@
-import { extendType, idArg, intArg, nonNull, objectType, stringArg } from "nexus";
+import { Prisma } from "@prisma/client";
+import { arg, enumType, extendType, idArg, inputObjectType, intArg, list, nonNull, objectType, stringArg } from "nexus";
+
+export const Sort = enumType({
+  name: "Sort",
+  members: ["asc", "desc"],
+});
+
+export const LinkOrderByInput = inputObjectType({
+  name: "LinkOrderByInput",
+  definition(t) {
+    t.field("description", { type: Sort });
+    t.field("url", { type: Sort });
+    t.field("createdAt", { type: Sort });
+  },
+});
 
 export const Link = objectType({
   name: "Link",
@@ -39,6 +54,7 @@ export const LinkQuery = extendType({
         filter: stringArg(),
         skip: intArg(),
         take: intArg(),
+        orderBy: arg({ type: list(nonNull(LinkOrderByInput)) }),
       },
       resolve(parent, args, ctx, info) {
         const where = args?.filter ? {
@@ -51,6 +67,7 @@ export const LinkQuery = extendType({
           where,
           skip: args?.skip as number | undefined,
           take: args?.take as number | undefined,
+          orderBy: args?.orderBy as Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput> | undefined,
         });
       },
     });
